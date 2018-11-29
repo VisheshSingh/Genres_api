ab3bd5571662909607b849f7f1a4da7df9a054dd
Vishesh <visheshsingh16@gmail.com>
2018-11-28 22:56:40 -0500

    Handling HTTP PUT request :robot:

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.get("/api/genres/:id", (req, res) => {
     res
       .status(400)
       .send("The requested genre is not available in our database records");
+    return;
   }
   res.send(genre);
 });
@@ -36,17 +37,10 @@ app.get("/api/genres/:id", (req, res) => {
 // MAKING POST REQUEST
 app.post("/api/genres", (req, res) => {
   // VALIDATION WITH JOI
-  const schema = {
-    name: Joi.string()
-      .min(3)
-      .required()
-  };
+  const { error } = validateGenre(req.body);
 
-  const result = Joi.validate(req.body, schema);
-  console.log(result);
-
-  if (result.error) {
-    res.status(400).send(result.error.details[0].message);
+  if (error) {
+    res.status(400).send(error.details[0].message);
     return;
   }
 
@@ -57,5 +51,43 @@ app.post("/api/genres", (req, res) => {
   genres.push(genre);
   res.send(genre);
 });
+
+// MAKING PUT REQUEST
+app.put("/api/genres/:id", (req, res) => {
+  // Look up for genre
+  // If not existing, return 404
+  const genre = genres.find(g => g.id === parseInt(req.params.id));
+  if (!genre) {
+    // BAD REQUEST
+    res
+      .status(404)
+      .send("The requested genre is not available in our database records");
+    return;
+  }
+  res.send(genre);
+  // Validate
+  // If invalid, return 400 - Bad request
+  const { error } = validateGenre(req.body);
+
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  // If valid, update course
+  genre.name = req.body.name;
+  // return updated course to the client
+  res.send(genre);
+});
+
+function validateGenre(course) {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
+
+  return Joi.validate(course, schema);
+}
 
 app.listen(3000, () => console.log("Listening to port 3000..."));
